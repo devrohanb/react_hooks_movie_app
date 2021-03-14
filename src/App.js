bd399c8a6f58086765e6571ed44b55bc7c536d62
devrohanb <dev.rohanb@gmail.com>
2021-03-14 11:39:07 +0530

    After impmementing the basic layout of the project and TMDB REST API

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Movie from "./components/Movie";
 // TODO: API links :
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e08dc74dbbfe0b96ad8d8f7a4699b50b&page=1";
-const IMG_API = "https://image.tmdb.org/t/p/w500";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?&api_key=e08dc74dbbfe0b96ad8d8f7a4699b50b&query=";
 
@@ -23,7 +22,17 @@ function App() {
       });
   }, []);
 
-  return <div>{movies.length > 0 && movies.map((movie) => <Movie />)}</div>;
+  return (
+    <>
+      <header>
+        <input className="search" type="text" placeholder="search.." />
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-const Movie = () => {
+const IMG_API = "https://image.tmdb.org/t/p/w500";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
-      <h1>Movies :</h1>
+      <img src={IMG_API + poster_path} alt={title} />
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+      <div className="movie-over">
+        <h2>Overview : </h2>
+        <p>{overview}</p>
+      </div>
     </div>
   );
 };
